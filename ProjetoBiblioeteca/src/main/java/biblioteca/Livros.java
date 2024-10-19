fcebcc3398293e9461c7f5f384efccf6b577361b
Harunno21 <arthur.dias@estudante.ifgoiano.edu.br>
2024-10-18 21:38:11 -0300

    Criação dos atributos da classe livro.
    Criei atributo: titulo, autor, ano da publicação e numero de exemplares.

--- ProjetoBiblioeteca/src/main/java/biblioteca/Livros.java
+++ ProjetoBiblioeteca/src/main/java/biblioteca/Livros.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package biblioteca;
 
-/**
- *
- * @author aluno
- */
+package biblioteca;
 public class Livros {
+    String titulo;
+    String autor;
+    String ano_publi;
+    int num_exemplares;
     
 }
