4658cfb978a173c1f861430c8525c9b34a97a036
Matheus-fel <matheus.bastos@estudante.ifgoiano.edu.br>
2024-10-18 21:57:36 -0300

    Adição do método construtor

--- ProjetoBiblioeteca/src/main/java/biblioteca/Usuarios.java
+++ ProjetoBiblioeteca/src/main/java/biblioteca/Usuarios.java
@@ -5,20 +5,23 @@ public class Usuarios {
     private String email;
     private String num_ident;
 
+    public Usuarios(String nome, String email, String num_ident){
+        this.nome = nome;
+        this.email = email;
+        this.num_ident = num_ident;
+    }
+
     public String getNome() {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public String getEmail() {
+        return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public String getNum_ident() {
+        return num_ident;
     }
-
-    public void setNum_ident(String num_ident) {
-        this.num_ident = num_ident;
-    }
-
+    
+    
 }
