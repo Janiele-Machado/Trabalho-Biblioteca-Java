49bd4a0f4428afa49fe44ec80a9d6cdb895111f1
Harunno21 <arthur.dias@estudante.ifgoiano.edu.br>
2024-10-18 22:16:13 -0300

    Get e set ano da publicação

--- ProjetoBiblioeteca/src/main/java/biblioteca/Livros.java
+++ ProjetoBiblioeteca/src/main/java/biblioteca/Livros.java
@@ -2,11 +2,12 @@
 package biblioteca;
 public class Livros {
     // Atributos da classe
-    String titulo;
-    String autor;
+    public String titulo;
+    public String autor;
     private int ano_publi;
     private int num_exemplares;
-   
+    public boolean emprestado = false;
+    
    // Construtores da classe livro
     public Livros (String titulo, String autor, int ano_publi, int num_exemplares){
         this.titulo = titulo;
@@ -14,4 +15,14 @@ public class Livros {
         this.ano_publi = ano_publi;
         this.num_exemplares = num_exemplares;
     }
+    //Criação de get e set para ano da publicação
+   
+    public int getAno_publi() {
+        return ano_publi;
+    }
+
+    public void setAno_publi(int ano_publi) {
+        this.ano_publi = ano_publi;
+    }
+    
 }
