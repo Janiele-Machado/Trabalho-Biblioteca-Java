50e3b75e4e00061cab5baed9b4a8d1f138971406
Janiele-Machado <janiele.farias@estudante.ifgoiano.edu.br>
2024-10-18 22:24:20 -0300

    Comecei um metodo para testar o emprestimo
     Comecei um metodo para testar se o livro foi emprestado ou nao

--- ProjetoBiblioeteca/src/main/java/biblioteca/EmpresDev.java
+++ ProjetoBiblioeteca/src/main/java/biblioteca/EmpresDev.java
@@ -2,12 +2,19 @@
 package biblioteca;
 
 public class EmpresDev {
-    Livros livro;
-    Usuarios usu;
+    public Livros livro;
+    public Usuarios usu;
+    private boolean emprest;
     
-    void empres (Livros liv,Usuarios usu){
-        
-        
+    void empres (Livros liv_titulo,Usuarios usu){
+         this.livro = liv_titulo;
+         this.usu=usu;
+         
+        if (this.livro.emprestado==false) {
+           //emprestado vai mudar de valor e tbm tem que colocar o usuario e o livro indisponivel; 
+            
+        }
+                
         
     }
 }
